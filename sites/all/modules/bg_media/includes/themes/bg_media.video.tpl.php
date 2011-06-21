2d3bdd61b7992995ccdbe72c5d6b06a2cb1bb885
Robbert van 't Westende <rvtwestende@totalactivemedia.nl>
2011-06-21 15:59:10 +0000

    Temporary update of B&G Media module
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18156 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

--- sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
+++ sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
@@ -1,13 +1,11 @@
-<!-- Begin VideoJS -->
-<?php dsm($items); if (count($items) > 0): ?>
-  <div class="video-js-box <?php print variable_get('videojs_skin', 'default') ?>" id="media">
-    <video id="media-video" class="video-js" width="480" height="360" controls="controls" preload="auto" poster="<?php print($poster) ?>">
-      <?php foreach ($items as $item): ?>
-        <?php $filepath = file_create_url($item['uri']); ?>
-        <source src="<?php print(file_create_url($item['uri'])) ?>" type="<?php print($item['videotype']) ?>" />
+<?php if(!empty($videos)) : ?>
+  <div class="video-js-box <?php print variable_get('videojs_skin', 'default'); ?>" id="media">
+    <video id="media-video" class="video-js" width="<?php echo $width; ?>" height="<?php echo $height; ?>" controls="controls" preload="auto" poster="<?php echo file_create_url($thumbnail['uri']); ?>">
+      <?php foreach ($videos as $video): ?>
+        <source src="<?php echo $video['video_url']; ?>" type="<?php echo $video['video_type']; ?>" />
       <?php endforeach; ?>
+
+      <?php echo $flash_video; ?>
     </video>
   </div>
 <?php endif; ?>
-<!-- End VideoJS -->
-
