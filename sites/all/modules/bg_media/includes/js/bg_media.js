8745517e6797d3cc323f7be24c59d025d61b8d63
Robbert van 't Westende <rvtwestende@totalactivemedia.nl>
2011-06-22 09:14:40 +0000

    Completed OpenImages video display
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18176 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

---
+++ sites/all/modules/bg_media/includes/js/bg_media.js
@@ -0,0 +1,17 @@
+(function($) {
+  $(document).ready(function(){
+    swfobject.embedSWF(Drupal.settings.bgMedia.playerSwf,
+        Drupal.settings.bgMedia.playerId,
+        Drupal.settings.bgMedia.playerWidth,
+        Drupal.settings.bgMedia.playerHeight,
+        "9.0.0",
+        null,
+        Drupal.settings.bgMedia.playerFlVars,
+        Drupal.settings.bgMedia.playerParams,
+        Drupal.settings.bgMedia.playerAttr,
+        function(e) {
+          VideoJS.setup();
+        }
+      );
+  });
+})(jQuery);
--- sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
+++ sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
@@ -1,6 +1,6 @@
 <?php if(!empty($videos)) : ?>
   <div class="video-js-box <?php print variable_get('videojs_skin', 'default'); ?>" id="media">
-    <video id="media-video" class="video-js" width="<?php echo $width; ?>" height="<?php echo $height; ?>" controls="controls" preload="auto" poster="<?php echo file_create_url($thumbnail['uri']); ?>">
+    <video id="media-video" class="video-js" width="<?php echo $width; ?>" height="<?php echo $height; ?>" controls="controls" preload="auto" poster="<?php echo $thumbnail; ?>">
       <?php foreach ($videos as $video): ?>
         <source src="<?php echo $video['video_url']; ?>" type="<?php echo $video['video_type']; ?>" />
       <?php endforeach; ?>
