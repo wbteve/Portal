9a8d3a144e4d0bb8df4380041131fee033f224e0
Robbert van 't Westende <rvtwestende@totalactivemedia.nl>
2011-06-22 13:57:47 +0000

    Added YouTube media support
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18202 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

--- sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
+++ sites/all/modules/bg_media/includes/themes/bg_media.video.tpl.php
@@ -1,5 +1,5 @@
-<?php if(isset($rtmp_video)) : ?>
-  <?php echo $rtmp_video; ?>
+<?php if(isset($video)) : ?>
+  <?php echo $video; ?>
 <?php elseif(!empty($videos)) : ?>
   <div class="video-js-box <?php print variable_get('videojs_skin', 'default'); ?>" id="media">
     <video id="media-video" class="video-js" width="<?php echo $width; ?>" height="<?php echo $height; ?>" controls="controls" preload="auto" poster="<?php echo $thumbnail; ?>">
