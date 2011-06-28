8ab4991d5fe5d333f5fede2ffba3a082f6387f22
Marc van Gend <mvgend@totalactivemedia.nl>
2011-06-28 10:58:47 +0000

    work on dossier display of search results and media
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18353 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

--- sites/all/themes/beeldgeluid/template.php
+++ sites/all/themes/beeldgeluid/template.php
@@ -174,3 +174,30 @@ function beeldgeluid_preprocess_block(&$variables, $hook) {
 function beeldgeluid_date_all_day_label() {
   return '';
 }
+
+/**
+ * Fix images sizes for external images.
+ * See http://api.drupal.org/api/drupal/modules--image--image.module/function/theme_image_style/7#comment-14839
+ */
+function beeldgeluid_image_style($variables) {
+  $style_name = $variables['style_name'];
+  $path = $variables['path'];
+
+  // theme_image() can only honor the $getsize parameter with local file paths.
+  // The derivative image is not created until it has been requested so the file
+  // may not yet exist, in this case we just fallback to the URL.
+  $style_path = image_style_path($style_name, $path);
+  if (!file_exists($style_path)) {
+    $style_path = image_style_url($style_name, $path);
+  }
+  $variables['path'] = $style_path;
+
+  if (is_file($style_path)) {
+    if (list($width, $height, $type, $attributes) = @getimagesize($style_path)) {
+      $variables['width'] = $width;
+      $variables['height'] = $height;
+    }
+  }
+  
+  return theme('image', $variables);
+}
