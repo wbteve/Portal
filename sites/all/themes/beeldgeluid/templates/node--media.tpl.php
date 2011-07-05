fa845837926716f65a09209c426dfb6d55106cc8
Marc van Gend <mvgend@totalactivemedia.nl>
2011-07-05 11:49:55 +0000

    code moved here from the deleted bg_dossier module
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18467 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

--- sites/all/themes/beeldgeluid/templates/node--media.tpl.php
+++ sites/all/themes/beeldgeluid/templates/node--media.tpl.php
@@ -79,38 +79,25 @@
  * @see zen_preprocess_node()
  * @see template_process()
  */
+dsm($content);
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
+    <?php print render($content['field_media_file']); ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($unpublished): ?>
-    <div class="unpublished"><?php print t('Unpublished'); ?></div>
-  <?php endif; ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
+    <?php print render($content['group_verbreding']); ?>
+  
+    <div class="content"<?php print $content_attributes; ?>>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+      ?>
     </div>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-
-  <?php print render($content['links']); ?>
-
-  <?php print render($content['comments']); ?>
-
+  
+    <?php print render($content['links']); ?>
+  
+    <?php print render($content['comments']); ?>
+  
 </div><!-- /.node -->
