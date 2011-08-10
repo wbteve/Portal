3533767458aac8a29f32c601b3504db921390306
Raymond Muilwijk <rmuilwijk@totalactivemedia.nl>
2011-08-10 09:43:50 +0000

    #32 aanpassingen in html in text modus niet opgeslagen
    git-svn-id: svn://svn.totalactivemedia.nl/beeldgeluid@18923 a5e0a7a9-93bf-8f48-8ca9-7e8460674ce7

--- sites/all/modules/bg_reference/js/bg_reference.js
+++ sites/all/modules/bg_reference/js/bg_reference.js
@@ -5,20 +5,27 @@
       $('.input-wrapper input[type="text"]', context)
         .bind('blur', handleInput)
         .trigger('blur');
-      
+
       // Handle input from tinyMCE
       tinyMCE.onAddEditor.add(function(sender, editor) {
         $('.input-wrapper textarea', context).each(function(){
           var _self = this;
-          
+
           if(editor.editorId == $(_self).attr('id')) {
-            editor.onSaveContent.add(function(editor){
-              $('input.bg_reference_content', $(_self).closest('fieldset')).val( editor.getContent() );
+            // In text mode
+            $(_self).change(function() {
+              console.log($(_self).val());
+              $('input.bg_reference_content', $(_self).closest('fieldset')).val( $(_self).val() );
+            });
+
+            // In WYSIWYG mode
+            editor.onSaveContent.add(function(editor) {
+                $('input.bg_reference_content', $(_self).closest('fieldset')).val( editor.getContent() );
             });
           }
         });
       });
-      
+
       // Apply color to color_back select options.
       $('select.bg_reference_color_back').each(function(index) {
         $(this).css({'color': '#fff', 'background-color': $(this).val()});
@@ -29,7 +36,7 @@
       });
     }
   }
-  
+
   if (!!Drupal.jsAC) { // this condition tests if autocomplete has been enabled
     /**
      * Hides the autocomplete suggestions.
@@ -50,7 +57,7 @@
       $(this.ariaLive).empty();
     };
   }
-  
+
   function handleInput(evt) {
     $('input.bg_reference_content', $(this).closest('fieldset')).val( $(this).val() );
   }
