ee51457c78eb3c1995deffaa2887d673a6058524
Eric Mulder <eric@limoengroen.nl>
2012-07-17 10:44:52 +0200

    Fixed some styling issues for Chrome

--- sites/all/modules/bg_reference/js/bg_reference_page.js
+++ sites/all/modules/bg_reference/js/bg_reference_page.js
@@ -1,8 +1,9 @@
 (function($) {
   Drupal.behaviors.xmlfeeditems = {
     attach: function(context) {
-      $('.xml-feed-items li li').hover(function(){
+      $('.xml-feed-items li li').hover(function(e){
         $(this).find('ul').show();
+        $(this).find('ul').css('top', e.pageY).css('left', e.pageX + 50);
       },function(){
         $(this).find('ul').hide();
       }
